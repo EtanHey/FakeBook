67ee12d1af70103e07c7eb9ec4a332e8b10defbe
Etan Joseph Heyman <etan@heyman.net>
2022-11-15 20:39:51 -0300

    Revert "holding fingers"
    This reverts commit 6bfb94add75314464928a12219594a8d7b687465.

--- dist/server/routes/userRoutes.js
+++ dist/server/routes/userRoutes.js
@@ -6,7 +6,6 @@ const userCont_1 = require("../controller/userCont");
 router
     .post('/add-new-user', userCont_1.addUser)
     .post('/login-user', userCont_1.loginUser)
-    .post('/search-users', userCont_1.searchUsers)
-    .get('/is-user-logged-in', userCont_1.isUserLoggedIn);
+    .post('/search-users', userCont_1.searchUsers);
 exports.default = router;
 //# sourceMappingURL=userRoutes.js.map
